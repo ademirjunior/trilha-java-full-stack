04664643038e958c86592df4ab4d806573a6f516
Ademir junior <ademiralcojr@gmail.com>
2023-10-23 17:51:37 -0300

    estudo serviços

---
+++ Modulo 4/estudo-servicos/src/app/components/sorteio-pessoas/sorteio-pessoas.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { SorteioService } from 'src/app/services/sorteio.service';
+
+@Component({
+  selector: 'app-sorteio-pessoas',
+  templateUrl: './sorteio-pessoas.component.html',
+  styleUrls: ['./sorteio-pessoas.component.css']
+})
+export class SorteioPessoasComponent {
+  texto: string = '';
+
+
+  constructor(private sorteioService: SorteioService){
+
+  }
+
+  sortear() {
+    const nomes: string[] = this.texto.split("\n");
+    const sorteado = this.sorteioService.sortear(nomes)
+    if (sorteado){
+      alert(`O nome sorteado foi:  ${sorteado}`);
+    } else {
+      alert('Digite os nomes no campo!')
+    }
+  }
+}
